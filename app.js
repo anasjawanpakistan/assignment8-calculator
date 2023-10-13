afe5e2e202c7a9c9b88372fb7cc9405bdf00225d
anasjawanpakistan <anasm0213@gmail.com>
2023-10-13 19:23:56 +0500

    JS

---
+++ app.js
@@ -0,0 +1,16 @@
+// function a1(){
+//     vara1 = document.getElementById(a1);
+//     console.log(a1);
+// }
+
+
+// function x2(){
+//     varx2 = document.getElementById(x2);
+//     console.log(x2);
+//
+
+
+// function x3(){
+//     varx3 = document.getElementById(x3);
+//     console.log(x3); 
+// }
